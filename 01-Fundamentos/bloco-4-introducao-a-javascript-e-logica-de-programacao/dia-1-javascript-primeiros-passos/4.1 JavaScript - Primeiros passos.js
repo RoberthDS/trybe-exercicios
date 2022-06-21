3b89af02970ed382242496f56b5f6cf9a54b52ca
Dniro15 <roberthgt158@gmail.com>
2022-06-21 18:51:29 -0300

    feito atividade 1

---
+++ 01-Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/4.1 JavaScript - Primeiros passos.js
@@ -0,0 +1,19 @@
+// Atividade 1
+const a = 40;
+const b = 50;
+
+let adicao = a + b;
+console.log(adicao);
+
+let subtracao = a - b;
+console.log(subtracao);
+
+let multiplicacao = a * b;
+console.log(multiplicacao);
+
+let divisao = a / b;
+console.log(divisao);
+
+let modulo = a % b;
+console.log(modulo);
+
