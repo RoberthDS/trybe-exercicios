41292c8c4d60bbcdf1d89e26ce28d04881132726
Dniro15 <roberthgt158@gmail.com>
2022-06-21 22:03:06 -0300

    feito atividade 7

--- 01-Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/4.1 JavaScript - Primeiros passos.js
+++ 01-Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/4.1 JavaScript - Primeiros passos.js
@@ -95,3 +95,28 @@ else if (peca.toLocaleLowerCase() === "rainha"){
 else {
   console.log("ERROR: Peça não existente");
 }
+
+// Atividade 7
+let nota = 104;
+
+if (nota >= 90 && nota <= 100) {
+  console.log("A");
+}
+else if (nota < 90 && nota >= 80) {
+  console.log("B");
+}
+else if (nota < 80 && nota>= 70){
+  console.log("C");
+}
+else if (nota >= 60 && nota < 70){
+  console.log("D");
+}
+else if (nota >= 50 && nota < 60) {
+  console.log("E");
+}
+else if (nota >= 0 && nota < 50) {
+  console.log("F");
+}
+else {
+  console.log("ERROR, Encerrando....");
+}
