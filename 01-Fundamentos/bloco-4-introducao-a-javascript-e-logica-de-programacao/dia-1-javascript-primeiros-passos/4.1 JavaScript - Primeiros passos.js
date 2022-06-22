675054e4024dfb906a1db993314490932e71d299
Dniro15 <roberthgt158@gmail.com>
2022-06-21 22:22:59 -0300

    feito atividade 8

--- 01-Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/4.1 JavaScript - Primeiros passos.js
+++ 01-Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/4.1 JavaScript - Primeiros passos.js
@@ -120,3 +120,15 @@ else if (nota >= 0 && nota < 50) {
 else {
   console.log("ERROR, Encerrando....");
 }
+
+// Atividade 8
+const n1 = 5;
+const n2 = 9;
+const n3 = 3;
+
+if (n1 % 2 === 0 || n2 % 2 === 0 || n3 % 2 === 0) {
+  console.log (true);
+}
+else {
+  console.log(false);
+}
