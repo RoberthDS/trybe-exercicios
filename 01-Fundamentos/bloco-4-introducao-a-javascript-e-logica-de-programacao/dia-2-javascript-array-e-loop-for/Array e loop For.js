3feaf8c29646a6b7c3ac18d3b56e2ee96b1dbdf0
Dniro15 <roberthgt158@gmail.com>
2022-06-22 19:09:59 -0300

    feito atividade 2

--- 01-Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/Array e loop For.js
+++ 01-Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/Array e loop For.js
@@ -1,6 +1,16 @@
 // Atividade 1
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for(let i = 0; i < numbers.length; i += 1) {
-    console.log(numbers[i]);
+for (let i = 0; i < numbers.length; i += 1) {
+  console.log(numbers[i]);
 }
+// Atividade 2
+let soma = 0;
+
+for (let i = 0; i < numbers.length; i += 1) {
+   soma += numbers[i];
+}
+  console.log("soma dos valores no array é: " + soma);
+
+
+
