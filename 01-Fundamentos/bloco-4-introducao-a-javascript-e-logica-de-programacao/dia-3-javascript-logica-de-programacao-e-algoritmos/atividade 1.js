d45b0727a2ca4ace34a236debbc8ebcf943dd515
Dniro15 <roberthgt158@gmail.com>
2022-06-24 18:26:49 -0300

    feito atividades

---
+++ 01-Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/atividade 1.js
@@ -0,0 +1,7 @@
+let fatorial = 10;
+
+let resultado;
+
+resultado = fatorial * (fatorial - 1) * (fatorial - 2) * (fatorial - 3) * (fatorial - 4) * (fatorial - 5) * (fatorial - 6) * (fatorial - 7) * (fatorial - 8) * (fatorial - 9);
+
+console.log(resultado);
