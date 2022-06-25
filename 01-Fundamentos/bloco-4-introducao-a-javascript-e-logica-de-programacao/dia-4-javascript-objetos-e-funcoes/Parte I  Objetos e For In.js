d18cd41196914bc508a541f850bd7ddfc0893692
Dniro15 <roberthgt158@gmail.com>
2022-06-24 23:09:24 -0300

    feito questão 5

--- 01-Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Parte I  Objetos e For In.js
+++ 01-Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Parte I  Objetos e For In.js
@@ -16,6 +16,26 @@ let info = {
     console.log(key);
   }
 
+  // Questão 4
   for (key in info) {
     console.log(info[key]);
   }
+
+  // Questão 5
+
+  let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+  };
+
+  for (let key in info){
+    if (key === "recorrente" && info[key] === "Sim" && info2[key] === "Sim") {
+        console.log("Ambos recorrentes");
+    }    
+    
+    else {
+            console.log(info[key] + " " + "e" + " "  + info2[key]);
+    }   
+}
