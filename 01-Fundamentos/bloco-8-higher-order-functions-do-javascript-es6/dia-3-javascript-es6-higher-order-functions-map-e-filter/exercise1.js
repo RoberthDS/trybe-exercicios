00ec3e3459dc51b3cb6262a69218de0f9c5705c0
RoberthDS <roberthgt158@gmail.com>
2022-07-25 21:27:56 -0300

    feito exercicio 1

--- 01-Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
+++ 01-Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
@@ -62,3 +62,36 @@ const books = [
   ];
   
   // Adicione o código do exercício aqui:
+  // 🚀 1 - Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA, Dica: Use a função map
+
+  // const expectedResult = [
+  //   'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+  //   'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+  //   'Fundação - Ficção Científica - Isaac Asimov',
+  //   'Duna - Ficção Científica - Frank Herbert',
+  //   'A Coisa - Terror - Stephen King',
+  //   'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+  // ];
+
+  // Como assimilei mesmo sendo mais linhas de codigo
+  function formatedBookNames() {
+    // escreva seu código aqui
+    return books.map((element) => {
+      return `${element.name} - ${element.genre} - ${element.author.name}`;
+    })
+  }; console.log('Priemira funtion'); 
+     console.log(formatedBookNames());
+
+  // // Com menos linhas de Codigo
+  function formatedBookNames() {
+    // escreva seu código aqui
+    return books.map((element) => `${element.name} - ${element.genre} - ${element.author.name}`);
+  }; console.log('segunda funtion');
+     console.log(formatedBookNames());
+
+  //fiz do zero com minha propria funtion criada
+const arrayString = (element) => books.map ((element) => `${element.name} - ${element.genre} - ${element.author.name}`);
+ console.log('terceira funtion'); 
+ console.log(arrayString());
+
+
