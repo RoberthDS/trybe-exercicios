d107c3ac2b35abaa8a490604f182c11768f29e5c
RoberthDS <roberthgt158@gmail.com>
2022-07-27 18:21:53 -0300

    teste

--- 01-Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise1.js
+++ 01-Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise1.js
@@ -6,6 +6,16 @@ const arrays = [
   [4, 5, 6],
 ];
 
+//Como compreendo melhor
 function flatten() {
   // escreva seu código aqui
-}
+  return arrays.reduce((acc, curr) => {
+    return acc.concat(curr)
+  } )
+} console.log(flatten());
+
+//com isso dando certo, consigo reduzir o codigo
+function flatten() {
+  
+  return arrays.reduce((acc, curr) => acc.concat(curr))
+} console.log(flatten());
